abd668aa9adba027f514ecce97d2d4eb527e1a15
MatheusAndreiczuk <93598422+MatheusAndreiczuk@users.noreply.github.com>
2022-05-10 17:17:56 -0300

    FolhaSalarial
    Finalizado

---
+++ FolhaSal/build/generated-sources/ap-source-output/vo/Tabela_.java
@@ -0,0 +1,35 @@
+package vo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-05-02T16:02:18")
+@StaticMetamodel(Tabela.class)
+public class Tabela_ { 
+
+    public static volatile SingularAttribute<Tabela, Double> tsf;
+    public static volatile SingularAttribute<Tabela, Double> vsf;
+    public static volatile SingularAttribute<Tabela, Double> ainss1;
+    public static volatile SingularAttribute<Tabela, Double> ainss3;
+    public static volatile SingularAttribute<Tabela, Double> tirrf4;
+    public static volatile SingularAttribute<Tabela, Double> ainss2;
+    public static volatile SingularAttribute<Tabela, Double> tirrf3;
+    public static volatile SingularAttribute<Tabela, Double> tirrf2;
+    public static volatile SingularAttribute<Tabela, Double> ainss4;
+    public static volatile SingularAttribute<Tabela, Double> tirrf1;
+    public static volatile SingularAttribute<Tabela, Double> dirrf2;
+    public static volatile SingularAttribute<Tabela, Double> dirrf4;
+    public static volatile SingularAttribute<Tabela, Double> dirrf3;
+    public static volatile SingularAttribute<Tabela, Double> tinss2;
+    public static volatile SingularAttribute<Tabela, Double> airrf5;
+    public static volatile SingularAttribute<Tabela, Double> tinss1;
+    public static volatile SingularAttribute<Tabela, Double> airrf4;
+    public static volatile SingularAttribute<Tabela, Double> dirrf5;
+    public static volatile SingularAttribute<Tabela, Double> tinss4;
+    public static volatile SingularAttribute<Tabela, Double> airrf3;
+    public static volatile SingularAttribute<Tabela, Double> tinss3;
+    public static volatile SingularAttribute<Tabela, Double> airrf2;
+    public static volatile SingularAttribute<Tabela, Double> dedpdep;
+
+}
